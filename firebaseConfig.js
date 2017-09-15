64e2b704408e51efb6247d09369f7f099450646a
Pao <pao.thongmotai@gmail.com>
2017-09-15 12:38:30 -0500

    Added config file

---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+var config = {
+	apiKey: "AIzaSyAo6NGH_36gvj0mSyVxug4_LmU9ToXu2ps",
+	authDomain: "simple-chat-ctpq8.firebaseapp.com",
+	databaseURL: "https://simple-chat-ctpq8.firebaseio.com",
+	projectId: "simple-chat-ctpq8",
+	storageBucket: "simple-chat-ctpq8.appspot.com",
+	messagingSenderId: "799371058702"
+};
